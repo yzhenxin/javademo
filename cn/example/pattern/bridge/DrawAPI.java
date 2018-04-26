d2c64cb8048ac4c72497d2dc78921fe14926ac8c
yuan.zhenxin <yuan.zhenxin@lifecycle.cn>
2018-04-26 16:38:33 +0800

    Bridge Pattern

---
+++ cn/example/pattern/bridge/DrawAPI.java
@@ -0,0 +1,5 @@
+package cn.example.pattern.bridge;
+
+public interface DrawAPI {
+	public void drawCircle(int radius, int x, int y);
+}
---
+++ cn/example/pattern/bridge/GreenCircle.java
@@ -0,0 +1,10 @@
+package cn.example.pattern.bridge;
+
+public class GreenCircle implements DrawAPI {
+
+	@Override
+	public void drawCircle(int radius, int x, int y) {
+		System.out.println("Drawing Circle[ green, radius: " + radius + ", x: " + x + ", " + y + "]");
+	}
+
+}
